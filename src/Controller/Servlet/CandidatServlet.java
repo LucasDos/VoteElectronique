ea6bc25454c33e71e28eed271b572fb5426668c6
LucasDos96 <dossantos.lucas12@gmail.com>
2020-04-23 14:33:04 +0200

    blabla

--- src/Controller/Servlet/CandidatServlet.java
+++ src/Controller/Servlet/CandidatServlet.java
@@ -1,6 +1,7 @@
 package Controller.Servlet;
 
 import Controller.DAO.CandidatDAO;
+import Controller.DAO.DBConnection;
 import Controller.DAO.PartiDAO;
 import Model.Candidat;
 import Model.Parti;
@@ -15,54 +16,23 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class CandidatServlet extends HttpServlet {
-    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         res.setContentType("text/html");
-        ArrayList<Candidat> candidats = null;
-        ArrayList<Parti> partis = null;
         try {
-            candidats = CandidatDAO.getAllCandidat();
-            partis = PartiDAO.getAllParti();
+            // Récupère les candidats et partis de la BDD
+            ArrayList<Candidat> candidats = CandidatDAO.getAllCandidat();
+            ArrayList<Parti> partis = PartiDAO.getAllParti();
+
+            if(candidats == null) { candidats = new ArrayList<Candidat>(); }
+            if(partis == null) { partis = new ArrayList<Parti>(); }
+
+            req.setAttribute("candidats", candidats);
+            PrintWriter out = res.getWriter();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        PrintWriter out = res.getWriter();
-        out.println("<head>\n" +
-                "    <title>Candidats</title>\n" +
-                "    <link rel=\"stylesheet\" href=\"styles.css\">\n" +
-                "</head>");
-
-        // Création du tableau des candidats
-        out.println("<html><body><h1>Liste des candidats</h1>");
-        out.println("<table> <tr> <th>Nom</th> <th>Prénom</th> <th>Parti</th> </tr>");
-
-        // Rempliassage du tableau
-        for(Candidat candidat : candidats){
-            out.println("<tr><td>" + candidat.getNom() + "</td> <td>" + candidat.getPrenom() + "</td>");
-
-            for(Parti parti : partis) {
-                if (candidat.getIdParti() == parti.getId()) {
-                    out.println("<td>" + parti.getNom() + "</td></tr>");
-                }
-            }
-        }
-        out.println("</table>");
-
-        //Création du tableau des partis
-        out.println("</table><br>" +
-                "<h1>Liste des partis</h1>" +
-                "<table>" +
-                "<tr>" +
-                "<th>Nom</th>" +
-                "<th>Nombre de candidats</th>" +
-                "<th>Siège</>");
-
-        for(Parti parti : partis) {
-            out.println("<tr><td>" + parti.getNom() + "</td> <td>" + parti.getNbInscrit() + "</td> <td>" +  parti.getSiege() + "</td></tr>");
-        }
-        out.println("</table>");
-
-        out.println("</body></html/>");
-
+        req.getRequestDispatcher("/candidats.jsp").forward(req,res);
     }
 }
